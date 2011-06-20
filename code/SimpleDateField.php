b824bb51f21679281fd4dc5132268c9b278c5624
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2011-06-20 06:12:34 +0000

    million bug fixes

--- code/SimpleDateField.php
+++ code/SimpleDateField.php
@@ -13,16 +13,17 @@ class SimpleDateField extends TextField {
 	 * @var array
 	 */
 	protected static $config = array(
-		'monthbeforeday' => true,
-		'righttitle' => "you can use 'tomorrow', 'next sunday' or '30-12-2012'",
-		'showcalendar' => false,
+		'showcalendar' => true,
+		'monthbeforeday' => false,
+		'righttitle' => "",
 		'dateformat' => "l j F Y", //see PHP Date function
 		'datavalueformat' => 'Y-m-d', //PHP data value format for saving into DB
-		'min' => null, //you can enter "today" or "next week" or any strtotime argument!
-		'max' => null,//you can enter "today" or "next week" or any strtotime argument!
+		'minDate' => "", //you can enter "today" or "next week" or any strtotime argument!
+		'maxDate' => "", //you can enter "today" or "next week" or any strtotime argument!
+		'locale' => "" //alternative locale for JS Calendar
 	);
 		static function set_config($a) {self::$config = $a;}
-		//static function add_config($key, $value) {self::$config[$key] = $value;}
+		static function add_config($key, $value) {self::$config[$key] = $value;}
 		//static function remove_config($key) {unset(self::$config[$key]);}
 		static function get_config() {return self::$config;}
 		static function get_config_item($name) {return self::$config[$name];}
@@ -36,55 +37,115 @@ class SimpleDateField extends TextField {
 			}
 		}
 		$rightTitle = $this->getConfig("righttitle");
+		if(!$rightTitle) {
+			$rightTitle = "  ";
+		}
 		$this->setRightTitle($rightTitle);
 	}
 
 	function Field() {
-		$settings = addslashes(serialize(self::get_config()));
-		$formID = $this->form->FormName();
-		$fieldID = $this->id();
-		$url = SimpleDateField_Controller::get_url();
+
+		//GENERAL
+		Requirements::javascript(THIRDPARTY_DIR . '/jquery/jquery.js');
+		Requirements::javascript("datefield_simplified/javascript/SimpleDateField.js");
 		$this->addExtraClass("simpledatefield");
-		$jsFuncField =<<<JS
-	jQuery('#$fieldID').change(
-		function() {
-			var id = jQuery(this).attr("id");
-			var value_value = jQuery(this).val();
-			url_value = "\/$url\/ajaxvalidation\/";
-			settings_value = '$settings';
-			SimpleDateFieldAjaxValidation(id, value_value, url_value, settings_value);
-		}
-	);
-JS;
-		$jsFuncGeneral =<<<JS
-function SimpleDateFieldAjaxValidation(id, value_value, url_value, settings_value) {
-	jQuery.ajax(
-		{
-			url: url_value,
-			data: ({value: value_value,settings: settings_value}),
-			success: function(returnData) {
-				array = returnData.split("|");
-				if(!array[1] || array[1] == "0") {
-					jQuery("#" + id).attr("value","?");
-					jQuery("label[for='"+id+"'].right").text(array[0]);
+
+		//CALENDAR STUFF
+		if($this->getConfig('showcalendar')) {
+			
+			Requirements::css(THIRDPARTY_DIR . '/jquery-ui-themes/smoothness/jquery.ui.all.css');
+			Requirements::javascript(SAPPHIRE_DIR . '/thirdparty/jquery-ui/jquery.ui.core.js');
+			Requirements::javascript(SAPPHIRE_DIR . '/thirdparty/jquery-ui/jquery.ui.datepicker.js');
+			
+			// Include language files (if required)
+			$myLang = $this->getConfig("locale");
+			$lang = i18n::get_locale();
+			$shortLang = substr($lang, 0,2);
+			$altLang = str_replace('_', '-', $lang);
+			// TODO Check for existence of locale to avoid unnecessary 404s from the CDN
+			$fileNameArray = array(
+				$myLang => sprintf('jquery.ui.datepicker-%s.js',$myLang),
+				$lang => sprintf('jquery.ui.datepicker-%s.js',$lang),
+				$shortLang => sprintf('jquery.ui.datepicker-%s.js',$shortLang),
+				$altLang => sprintf('jquery.ui.datepicker-%s.js',$altLang)
+			);
+			foreach($fileNameArray as $key => $fileName) {
+				$fileName =   THIRDPARTY_DIR . '/jquery-ui/i18n/' . $fileName;
+				if(file_exists(Director::baseFolder() . "/" . $fileName)) {
+					Requirements::javascript($fileName);
+					self::add_config("locale", $key);
+					break;
 				}
-				else {
-					jQuery("label[for='"+id+"'].right").text(" ");
-					jQuery("#" + id).attr("value",array[0]);
-				}
-				
 			}
 		}
-	);
-}
-JS;
 
-		Requirements :: customScript($jsFuncGeneral, 'func_SimpleDateField');		
+		//CUSTOM STUFF
+		$fieldID = $this->id();
+		//TO DO:::::: THIS DOES NOT APPEAR TO BE PROPER JSON !
+		
+		$settingsArray = array();
+		$jsConfigArray = self::convert_array_between_string_and_proper_value(self::get_config(), false);
+		foreach($jsConfigArray as $key => $value) {
+			if(is_string($value)) {
+				$value = "'".Convert::raw2js($value)."'";
+			}
+			$settingsArray []= "$key : ". $value;
+		}
+		$settings = "{".implode(",",$settingsArray)."}";
+		$url = Convert::raw2js(SimpleDateField_Controller::get_url()."/ajaxvalidation/");
+		$jsFuncField =<<<JS
+simpledatefield_$fieldID = $settings;
+jQuery('#$fieldID').live(
+	"change",
+	function() {
+		var id = jQuery(this).attr("id");
+		var value_value = jQuery(this).val();
+		url_value = '$url';
+		SimpleDateFieldAjaxValidation.sendInput(id, value_value, url_value, simpledatefield_$fieldID);
+	}
+);
+JS;
+		if($this->getConfig('showcalendar')) {
+			$jsSettingsArray = Array();
+			if($this->getConfig('monthbeforeday')) {
+				$jsFormat = 'mm/dd/yy';
+				$phpFormat = 'm/d/Y';
+			}
+			else {
+				$jsFormat = 'dd-mm-yy';
+				$phpFormat = 'd-m-Y';
+			}
+			$jsSettingsArray[] = " dateFormat: '$jsFormat'";
+			if($maxDate = self::get_config_item('maxDate')) {
+				$maxDate = strtotime($maxDate);
+				if($maxDate) {
+					$jsSettingsArray[] = " maxDate: '".date($phpFormat, $maxDate)."'";
+				}
+			}
+			if($minDate = self::get_config_item('minDate')) {
+				$minDate = strtotime($minDate);
+				if($minDate) {
+					$jsSettingsArray[] = " minDate: '".date($phpFormat, $minDate)."'";
+				}
+			}
+			$jsSettingsString = "{".implode(",", $jsSettingsArray)."}";
+			$jsFuncField .=<<<JS
+jQuery("#$fieldID").live(
+	"click",
+	function() {
+		if(simpledatefield_$fieldID.locale && jQuery.datepicker.regional[simpledatefield_$fieldID.locale]) {
+			config = jQuery.extend(simpledatefield_$fieldID, jQuery.datepicker.regional[simpledatefield_$fieldID.locale], {});
+		}
+		jQuery('#$fieldID').datepicker($jsSettingsString);
+		jQuery('#$fieldID').datepicker('show');
+	}
+);
+JS;
+		}
 		Requirements :: customScript($jsFuncField, 'func_SimpleDateField'.$fieldID);		
 		$html = parent::Field();
 		return $html;
 	}
-
 
 	/**
 	 * @return String ISO 8601 date, suitable for insertion into database
@@ -139,53 +200,99 @@ JS;
 		return self::$config[$name];
 	}
 
-	static function convert_to_ts_or_error($rawInput) {
+	public static function convert_to_ts_or_error($rawInput) {
+		//return strtotime("24-06-2012");
+		$tsOrError = null;
 		if(self::get_config_item('monthbeforeday')) {
 			$cleanedInput = str_replace("-", "/", $rawInput);
 		}
 		else {
 			$cleanedInput = str_replace("/", "-", $rawInput);
 		}
-		// min/max - Assumes that the date value was valid in the first place
 		if($cleanedInput) {
 			$tsOrError = intval(strtotime($cleanedInput));
 		}
 		if(is_numeric($tsOrError) && $tsOrError > 0) {
-			if($min = self::get_config_item('min')) {
-				$minDate = strtotime($min);
-				if($minDate > $tsOrError) {
-					$tsOrError = sprintf(_t('SimpleDateField.VALIDDATEMINDATE', "Your date can not be before %s."),date(self::get_config_item('dateformat'), $minDate));
+			if($minDate = self::get_config_item('minDate')) {
+				$minDate = strtotime($minDate);
+				if($minDate) {
+					if($minDate > $tsOrError) {
+						$tsOrError = sprintf(_t('SimpleDateField.VALIDDATEMINDATE', "Your date can not be before %s."),date(self::get_config_item('dateformat'), $minDate));
+					}
 				}
 			}
-			if($max = self::get_config_item('max')) {
+			if($maxDate = self::get_config_item('maxDate')) {
 				// ISO or strtotime()
-				$maxDate = strtotime($max);
-				if($maxDate < $tsOrError) {
-					$tsOrError = sprintf(_t('DateField.VALIDDATEmaxDATE', "Your date can not be after %s."),date(self::get_config_item('dateformat'), $maxDate));
+				$maxDate = strtotime($maxDate);
+				if($maxDate) {
+					if($maxDate < $tsOrError) {
+						$tsOrError = sprintf(_t('SimpleDateField.VALIDDATEMAXDATE', "Your date can not be after %s."),date(self::get_config_item('dateformat'), $maxDate));
+					}
 				}
 			}
 		}
 		if(!$tsOrError) {
 			if(!trim($rawInput)) {
-				$tsOrError = sprintf(_t('DateField.VALIDDATEmaxDATE', "You need to enter a valid date."),$rawInput);
+				$tsOrError = sprintf(_t('SimpleDateField.VALIDDATEDATE', "You need to enter a valid date."),$rawInput);
 			}
 			else {
-				$tsOrError = sprintf(_t('DateField.VALIDDATEmaxDATE', "We did not understand the date you entered '%s'."),$rawInput);
+				$tsOrError = sprintf(_t('SimpleDateField.VALIDDATEDATE', "We did not understand the date you entered '%s'."),$rawInput);
 			}
 		}
 		return $tsOrError;
 	}
 
-	static function convert_to_fancy_date($rawInput) {
+	public static function convert_to_fancy_date($rawInput) {
 		$tsOrError = self::convert_to_ts_or_error($rawInput);
 		if(is_numeric($tsOrError) && $tsOrError) {
-			return date(self::get_config_item("dateformat"), $tsOrError )."|1";
+			return Convert::raw2json(
+				array(
+					"value" => date(self::get_config_item("dateformat"), $tsOrError ),
+					"success" => 1
+				)
+			);
 		}
 		else {
-			return "$tsOrError|0";
+			return Convert::raw2json(
+				array(
+					"value" =>  $tsOrError,
+					"success" => 0
+				)
+			);
 		}
 	}
 
+	public function convert_array_between_string_and_proper_value($input, $fromString = true) {
+		$array = array(
+			"false" => false,
+			"true" => true,
+			"null" => null
+		);
+		$output = array();
+		if($input && count($input)) {
+			foreach($input as $key => $value) {
+				if(is_array($value)) {
+					$value = SimpleDateField::convert_array_between_string_and_proper_value($value, $fromString);
+				}
+				else {
+					if($fromString) {
+						if(isset($array[$value])) {
+							$value = $array[$value];
+						}
+					}
+					else {
+						$newValue = array_search($value, $array, true);
+						if($newValue) {
+							$value = $newValue;
+						}
+					}
+				}
+				$output[$key] = $value;
+			}
+		}
+		return $output;
+
+	}
 }
 
 
@@ -201,10 +308,11 @@ class SimpleDateField_Controller extends Controller {
 			$rawInput = ($_GET["value"]);
 		}
 		if(isset($_GET["settings"])) {
-			SimpleDateField::set_config(unserialize(($_GET["settings"])));
+			SimpleDateField::set_config(SimpleDateField::convert_array_between_string_and_proper_value($_GET["settings"], true));
 		}		
 		return SimpleDateField::convert_to_fancy_date($rawInput);
 	}
+
 
 }
 
@@ -252,12 +360,13 @@ class SimpleDateField_Editable extends EditableFormField {
 	public function getFormField() {
 		$field = new SimpleDateField($this->Name, $this->Title);
 		if($this->getSetting('OnlyPastDates')) {
-			$field->setConfig("max", "today");
+			$field->setConfig("maxDate", "today");
 		}
 		elseif($this->getSetting('OnlyFutureDates')) {
-			$field->setConfig("min", "today");
+			$field->setConfig("minDate", "today");
 		}
 		if($this->getSetting('MonthBeforeDay')) {
+			$field->setConfig("dateformat", 'l F j Y');
 			$field->setConfig("monthbeforeday", true);
 		}
 		if($this->getSetting('ExplanationForEnteringDates')) {
